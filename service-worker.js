308ee91c757c050bde4919fac8221c9cf191e37b
purogramken-cell <purogramken@gmail.com>
2026-04-11 01:19:53 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,9 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('odakyu-go-v2').then(cache => cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.registration.unregister().then(() => self.clients.matchAll()).then((clients) => {
+    clients.forEach((client) => client.navigate(client.url));
+  }));
 });
