92100c3868897e09bd4fb39fac101e9cdefd209c
purogramken-cell <purogramken@gmail.com>
2026-04-11 01:09:13 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,6 @@
-const CACHE_NAME = 'train-numbers-v1';
-const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('odakyu-go-v2').then(cache => cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
